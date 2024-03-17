d604853bdaa3d803e4190fca5d50bcd0220d82f8
epsxy <7492974+epsxy@users.noreply.github.com>
2024-03-18 00:25:20 +0100

    feat: update layout

--- layouts/dacty_manuform/keymap.c
+++ layouts/dacty_manuform/keymap.c
@@ -24,8 +24,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          KC_LGUI, KC_LALT,                                                         KC_RALT, FR_EQL,
                              // SPC   ,  BSPC        ,                                     DEL   , ENTER  ,
                                 KC_SPC, KC_BSPC      ,                                     KC_DEL, KC_ENT ,
-                                        _______, TO(LAYER_1),               TO(LAYER_2),_______,
-                                        _______,_______,                        _______,_______
+                                     // BOTTOM RIGHT, TOP RIGHT            // TOP LEFT, BOTTOM LEFT
+                                        _______, TO(LAYER_1),                 TO(LAYER_2) ,_______,
+                                     // BOTTOM LEFT, TOP LEFT              // TOP RIGHT, BOTTOM RIGHT
+                                        _______, OSL(LAYER_1),                OSL(LAYER_2),_______
     ),
     [_LAYER_1] = LAYOUT_5x6(
         KC_ESC , KC_F1  , KC_F2  , KC_F3   , KC_F4 , KC_F5 ,                         KC_F6  , KC_F7 , KC_F8 , KC_F9 , KC_F10, KC_F11,
