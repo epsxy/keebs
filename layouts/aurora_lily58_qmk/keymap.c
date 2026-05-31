e571d36d8f0c95fc3761759542ce17a8dac0f515
epsxy <epsxy@users.noreply.github.com>
2026-05-31 22:40:09 +0200

    fix: make aurora lily58 oled layout readable

--- layouts/aurora_lily58_qmk/keymap.c
+++ layouts/aurora_lily58_qmk/keymap.c
@@ -5,6 +5,11 @@ enum layers {
     _BASE,
     _LOWER,
     _RAISE,
+    _APP,
+};
+
+enum custom_keycodes {
+    LOWER_APP = SAFE_RANGE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -13,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC,
         KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    FR_M,    FR_QUOT,
         KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE, MO(_RAISE), KC_N,  FR_COMM, FR_SCLN, FR_COLN, FR_EXLM, FR_EQL,
-                                   KC_LALT, KC_RALT, KC_LGUI, KC_SPC,     KC_BSPC, KC_ENT,  XXXXXXX, LALT(KC_SPC)
+                                   KC_LALT, KC_RALT, KC_LGUI, KC_SPC,     KC_BSPC, KC_ENT,  LOWER_APP, LALT(KC_SPC)
     ),
 
     [_LOWER] = LAYOUT(
@@ -31,7 +36,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LCTL(LSFT(LGUI(FR_QUOT))), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
+
+    [_APP] = LAYOUT(
+        KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        LGUI(KC_TAB), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, HYPR(KC_T),              XXXXXXX, XXXXXXX, HYPR(KC_I), XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, HYPR(KC_S), XXXXXXX, HYPR(KC_F), HYPR(KC_G),             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MCTL, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, HYPR(KC_C), XXXXXXX, HYPR(KC_B), LGUI(KC_GRV), LGUI(KC_TAB), XXXXXXX, LCTL(KC_LEFT), LCTL(KC_DOWN), LCTL(KC_UP), LCTL(KC_RIGHT), XXXXXXX,
+                                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
 };
+
+static uint16_t lower_app_timer;
+static bool     lower_app_pressed;
+static bool     lower_app_used;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (lower_app_pressed && keycode != LOWER_APP && record->event.pressed) {
+        lower_app_used = true;
+    }
+
+    switch (keycode) {
+        case LOWER_APP:
+            if (record->event.pressed) {
+                lower_app_timer   = timer_read();
+                lower_app_pressed = true;
+                lower_app_used    = false;
+                layer_on(_LOWER);
+            } else {
+                layer_off(_LOWER);
+                lower_app_pressed = false;
+
+                if (!lower_app_used && timer_elapsed(lower_app_timer) < TAPPING_TERM) {
+                    set_oneshot_layer(_APP, ONESHOT_START);
+                }
+            }
+            return false;
+    }
+
+    return true;
+}
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
@@ -49,69 +92,8 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 #endif
 
 #ifdef OLED_ENABLE
-static const uint8_t PROGMEM digit_glyphs[][7] = {
-    {0b00100, 0b01100, 0b00100, 0b00100, 0b00100, 0b00100, 0b01110},
-    {0b01110, 0b10001, 0b00001, 0b00010, 0b00100, 0b01000, 0b11111},
-    {0b11110, 0b00001, 0b00001, 0b01110, 0b00001, 0b00001, 0b11110},
-};
-
 static uint8_t current_layer(void) {
     return get_highest_layer(layer_state | default_layer_state);
-}
-
-static uint8_t layer_number(uint8_t layer) {
-    switch (layer) {
-        case _LOWER:
-            return 2;
-        case _RAISE:
-            return 3;
-        default:
-            return 1;
-    }
-}
-
-static void draw_scaled_digit(uint8_t digit, uint8_t x, uint8_t y, uint8_t scale) {
-    if (digit < 1 || digit > 3) {
-        return;
-    }
-
-    for (uint8_t row = 0; row < 7; row++) {
-        uint8_t bits = pgm_read_byte(&digit_glyphs[digit - 1][row]);
-
-        for (uint8_t col = 0; col < 5; col++) {
-            if (!(bits & (1 << (4 - col)))) {
-                continue;
-            }
-
-            for (uint8_t dy = 0; dy < scale; dy++) {
-                for (uint8_t dx = 0; dx < scale; dx++) {
-                    oled_write_pixel(x + (col * scale) + dx, y + (row * scale) + dy, true);
-                }
-            }
-        }
-    }
-}
-
-static void draw_art_square(uint8_t x, uint8_t y, uint8_t size) {
-    for (uint8_t row = 0; row < size; row++) {
-        for (uint8_t col = 0; col < size; col++) {
-            bool border = row == 0 || col == 0 || row == size - 1 || col == size - 1;
-            bool weave  = ((row + col) % 5) == 0 || ((row + (size - col)) % 7) == 0;
-
-            oled_write_pixel(x + col, y + row, border || weave);
-        }
-    }
-}
-
-static void draw_art_rect(uint8_t x, uint8_t y, uint8_t width, uint8_t height) {
-    for (uint8_t row = 0; row < height; row++) {
-        for (uint8_t col = 0; col < width; col++) {
-            bool border = row == 0 || col == 0 || row == height - 1 || col == width - 1;
-            bool trace  = ((col + (row * 2)) % 9) == 0 || (((width - col) + row) % 13) == 0;
-
-            oled_write_pixel(x + col, y + row, border || trace);
-        }
-    }
 }
 
 static bool is_left_display(void) {
@@ -122,38 +104,94 @@ static bool is_left_display(void) {
 #endif
 }
 
-static void render_left_oled(uint8_t number) {
-    oled_set_cursor(8, 0);
-    oled_write_P(PSTR("epsxy"), false);
-    draw_scaled_digit(number, 56, 9, 3);
-    draw_art_square(4, 13, 18);
+static void render_layer_name(uint8_t layer) {
+    switch (layer) {
+        case _LOWER:
+            oled_write_ln_P(PSTR("LOWER"), false);
+            break;
+        case _RAISE:
+            oled_write_ln_P(PSTR("RAISE"), false);
+            break;
+        case _APP:
+            oled_write_ln_P(PSTR("APP  "), false);
+            break;
+        default:
+            oled_write_ln_P(PSTR("BASE "), false);
+            break;
+    }
 }
 
-static void render_right_oled(uint8_t number) {
-    oled_set_cursor(0, 0);
-    oled_write_P(PSTR("L"), false);
-    oled_write_char('0' + number, true);
-    draw_art_rect(18, 8, 104, 22);
+static void render_layer_row(uint8_t current, uint8_t layer, const char *name) {
+    oled_write_ln_P(name, current == layer);
+}
+
+static void render_mods(void) {
+    uint8_t mods = get_mods() | get_oneshot_mods();
+
+    oled_write_P(PSTR("S"), mods & MOD_MASK_SHIFT);
+    oled_write_P(PSTR("C"), mods & MOD_MASK_CTRL);
+    oled_write_P(PSTR("A"), mods & MOD_MASK_ALT);
+    oled_write_P(PSTR("G"), mods & MOD_MASK_GUI);
+    oled_write_ln_P(PSTR(" "), false);
+}
+
+static void render_locks(void) {
+    led_t led_state = host_keyboard_led_state();
+
+    oled_write_ln_P(PSTR("CAPS "), led_state.caps_lock);
+    oled_write_ln_P(PSTR("NUM  "), led_state.num_lock);
+}
+
+static void render_left_oled(uint8_t layer) {
+    oled_write_ln_P(PSTR("epsxy"), false);
+    oled_write_ln_P(PSTR("-----"), false);
+    render_layer_row(layer, _BASE, PSTR("BASE "));
+    render_layer_row(layer, _LOWER, PSTR("LOWER"));
+    render_layer_row(layer, _RAISE, PSTR("RAISE"));
+    render_layer_row(layer, _APP, PSTR("APP  "));
+    oled_write_ln_P(PSTR("-----"), false);
+    oled_write_ln_P(PSTR("MODS "), false);
+    render_mods();
+    render_locks();
+}
+
+static void render_right_oled(uint8_t layer) {
+    oled_write_ln_P(PSTR("LAYER"), false);
+    oled_write_ln_P(PSTR("-----"), false);
+    render_layer_name(layer);
+    oled_write_ln_P(PSTR("-----"), false);
+    oled_write_ln_P(PSTR("HOLD "), lower_app_pressed);
+    oled_write_ln_P(PSTR("APP  "), layer == _APP);
+    oled_write_ln_P(PSTR("LOWER"), layer == _LOWER);
+    oled_write_ln_P(PSTR("RAISE"), layer == _RAISE);
 }
 
 bool oled_task_user(void) {
-    static uint8_t last_number = 0;
-    static bool    last_left   = false;
-    uint8_t        number      = layer_number(current_layer());
-    bool           left        = is_left_display();
+    static uint8_t last_layer = 255;
+    static uint8_t last_mods  = 255;
+    static led_t   last_leds  = {0};
+    static bool    last_left  = false;
+    static bool    last_hold  = false;
+    uint8_t        layer      = current_layer();
+    uint8_t        mods       = get_mods() | get_oneshot_mods();
+    led_t          leds       = host_keyboard_led_state();
+    bool           left       = is_left_display();
 
-    if (number == last_number && left == last_left) {
+    if (layer == last_layer && mods == last_mods && leds.raw == last_leds.raw && left == last_left && lower_app_pressed == last_hold) {
         return false;
     }
 
-    last_number = number;
-    last_left   = left;
+    last_layer = layer;
+    last_mods  = mods;
+    last_leds  = leds;
+    last_left  = left;
+    last_hold  = lower_app_pressed;
     oled_clear();
 
     if (left) {
-        render_left_oled(number);
+        render_left_oled(layer);
     } else {
-        render_right_oled(number);
+        render_right_oled(layer);
     }
 
     return false;
